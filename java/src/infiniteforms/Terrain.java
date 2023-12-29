baba16ee9e0ff0eba0f5fbf78b7ccf6882605200
Bryan Duggan <skooter500@gmail.com>
2023-12-29 00:23:06 +0000

    bla

--- java/src/infiniteforms/Terrain.java
+++ java/src/infiniteforms/Terrain.java
@@ -24,13 +24,15 @@ public class Terrain extends Poly {
 
     public void render() {
 
-        flying -= v.getSmoothedAmplitude() * v.spe;
+        flying -= v.spe * 0.01f;
 
         float yoff = flying;
+        //float a = (v.getSmoothedAmplitude() * (v.map(v.mul, 0, 100, 0, 1)  + v.bas)) * 200;
+        float a = 200 * v.bas;
         for (int y = 0; y < rows; y++) {
             float xoff = 0;
             for (int x = 0; x < cols; x++) {
-                terrain[x][y] = v.map(v.noise(xoff, yoff), 0, 1, -100, 100);
+                terrain[x][y] = v.map(v.noise(xoff, yoff), 0, 1, -a, a);
                 xoff += 0.2;
             }
             yoff += 0.2;
@@ -38,13 +40,19 @@ public class Terrain extends Poly {
 
         v.noFill();
 
-        v.translate(v.width / 2, v.height / 2 + 50, -1000);
+        v.translate(v.width / 2, v.height / 2 - 200, -500);
+        v.rotateX(v.pit);
+        v.rotateY(v.yaw);
+        v.rotateZ(v.rol);
+
         v.rotateX(v.PI / 3);
         v.translate(-w / 2, -h / 2);
         for (int y = 0; y < rows - 1; y++) {
             v.beginShape(v.TRIANGLE_STRIP);
             for (int x = 0; x < cols; x++) {
-                v.stroke(v.map(x + y + offs, 0, rows + cols, 255, 0) % 255, 255, 255);
+                float c3 = v.map(x + y + offs, 0, rows + cols, 255, 0);
+                c3 = v.hueShift(c3); 
+                v.stroke(c3, 255, 255, v.alp);
                 v.vertex(x * scl, y * scl, terrain[x][y]);
                 v.vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
                 // rect(x*scl, y*scl, scl, scl);
